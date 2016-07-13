d8322a3fcaafc310ee32fe6a896b9a83c4ea8840
lisbeymella <20142172@itla.edu.do>
2016-07-13 19:36:28 -0400

    Jukebox

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
+#include "Reproduccion.h"
+#include <stdio.h>
 using namespace std;
 
 //TODO: Implement this code from the scratch
-/*
- * At this point your expected to be able to create your code from the scratch without any guidance.
- *
- */
+int main()
+{
+  Reproduction* list = new Reproduction();
+  Reproduction:: Start head;
+  head=list->createNodo();
+  int entry1;
+  cin>>entry1;
+  list->add(head,entry1);
+     int entry2;
+  cin>>entry2;
+  lista->add(head,entry2);
+     int entry3;
+  cin>>entry3;
+  lista->add(head,entry3);
+     /*int entry4;
+  cin>>entry4;
+    cout<<list->remove(head,entry3);*/
+  list->list(head);
+
+
+}
+
 
 int main() {
 	cout << "Implement your code" << endl;
